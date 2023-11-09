42b2ee652113b92616f1c564939494177537a87a
Maits27 <maitiurlo@gmail.com>
2023-11-09 01:03:12 +0100

    Funciona hasta menu

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, json, request, redirect, session, url_for
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import hashlib
 
 app = Flask(__name__)
+
+app.secret_key = '123456789'
 
 app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'root'
@@ -12,35 +14,7 @@ app.config['MYSQL_DB'] = 'database'
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['POST', 'GET'])
-def login():
-    valor_error = ''
-    if request.method == 'POST':
-        email = request.form["email2"]
-        contra = request.form["contrasena2"]
-        
-        # Establecer la conexión a la base de datos MySQL (asegúrate de que MySQL esté configurado)
-        
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        
-        cursor.execute(f'SELECT * FROM usuarios WHERE email = {email} AND contrasena = {contra}')
-        
-        user = cursor.fetchone()
-        
-        cursor.close()
-        
-        if user:
-            session['username'] = user['nombre']
-            session['email'] = email
-            session['ID_USER'] = user['id']
-            return redirect(url_for('user_menu'))
-        else:
-            valor_error = 'No existe el usuario.'
-            return redirect(url_for('login', error=valor_error))
-
-    return render_template('index.html', error=valor_error)
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     valor_error = ''
     if request.method == 'POST':
@@ -59,20 +33,72 @@ def register():
         contraseña_hash = sha256.hexdigest()
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(f'INSERT INTO usuarios (id, nombre, email, contrasena) VALUES ({id}, {nombre}, {email}, {contraseña_hash})')
-        user = cursor.fetchone()
-        cursor.close()
+        cursor.execute('SELECT * FROM usuarios WHERE id = %s', (id,))
+        user=cursor.fetchone()
 
         if user:
             valor_error = 'Ya existe un usuario con ese email y nombre.'
-            return redirect(url_for('login', error=valor_error))
+            return redirect(url_for('register', error=valor_error))
         else:
+            cursor.execute('INSERT INTO usuarios (id, nombre, email, contrasena) VALUES (%s, %s, %s, %s)', (id, nombre, email, contraseña_hash,))
+            mysql.connection.commit()
+
+            session['username'] = nombre
+            session['email'] = email
+            session['ID_USER'] = id
+
+            return redirect(url_for('user_menu'))
+        
+    return render_template('index.html', error=valor_error)
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    valor_error = ''
+    if request.method == 'POST':
+        email = request.form["email2"]
+        contra = request.form["contrasena2"]
+        
+        # Establecer la conexión a la base de datos MySQL (asegúrate de que MySQL esté configurado)
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        
+        cursor.execute('SELECT * FROM usuarios WHERE email = %s AND contrasena = %s', (email, contra,))
+        
+        user = cursor.fetchone()
+        
+        cursor.close()
+        
+        if user:
             session['username'] = user['nombre']
             session['email'] = email
             session['ID_USER'] = user['id']
             return redirect(url_for('user_menu'))
-        
+        else:
+            valor_error = 'No existe el usuario.'
+            return redirect(url_for('login', error=valor_error))
+
     return render_template('index.html', error=valor_error)
+
+
+
+@app.route('/user_menu', methods=['POST', 'GET'])
+def user_menu():
+    id=session['ID_USER']
+    proyecto_json = ''
+    
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT proyecto.* FROM proyecto JOIN proyectosUsuario ON proyecto.id = proyectosUsuario.IdProyecto WHERE proyectosUsuario.IdUsuario = %s', (id,))
+    proyecto = cursor.fetchone()
+    cursor.close()
+
+    if proyecto:
+        proyecto_json = json.dumps(proyecto, ensure_ascii=False)
+    
+    datos = {
+        'user': session['username'],
+        'proyectos': proyecto_json 
+    }
+    return render_template('user_menu.html', datos=datos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.sql
+++ database.sql
@@ -1,24 +1,24 @@
 
 -- Crear la tabla de usuarios
 CREATE TABLE usuarios (
-    id CHAR(32) PRIMARY KEY,
+    id CHAR(64) PRIMARY KEY,
     nombre VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     contrasena VARCHAR(255)
 );
 
 -- Crear la tabla de grupos
-CREATE TABLE grupo (
-    id CHAR(32) PRIMARY KEY,
+CREATE TABLE proyecto (
+    id CHAR(64) PRIMARY KEY,
     nombre VARCHAR(255) UNIQUE,
     contrasena VARCHAR(255)
 );
 
 -- Crear la tabla intermedia gruposUsuario
-CREATE TABLE gruposUsuario (
-    id CHAR(32) PRIMARY KEY,
-    IdUsuario CHAR(32),
-    IdGrupo CHAR(32),
+CREATE TABLE proyectosUsuario (
+    id CHAR(64) PRIMARY KEY,
+    IdUsuario CHAR(64),
+    IdProyecto CHAR(64),
     FOREIGN KEY (IdUsuario) REFERENCES usuarios(id),
-    FOREIGN KEY (IdGrupo) REFERENCES grupo(id)
+    FOREIGN KEY (IdProyecto) REFERENCES proyecto(id)
 );
